91ab64db7c9b41ff4adb5e264961003a8d289472
Rostislav <rvssv@bk.ru>
2022-01-13 14:36:37 +0300

    added feature_characters_header

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -6,8 +6,11 @@
                 <button class="search-button">search</button>
             </div>
         </div>
-        <CharactersGrid v-if="characters && characters.length > 0" :characters="characters"/>
-        <SkeletonAnimation v-else/>
+        <div>
+            <CharactersHeader/>
+            <CharactersGrid v-if="characters && characters.length > 0" :characters="characters"/>
+            <SkeletonAnimation v-else/>
+        </div>
     </main>
 </template>
 
@@ -15,10 +18,11 @@
     import CharactersGrid from "@/components/CharactersGrid";
     import SkeletonAnimation from "@/components/SkeletonAnimation";
     import { mapGetters } from "vuex"
+    import CharactersHeader from "@/components/CharactersHeader";
 
     export default {
         name: "Main",
-        components: { SkeletonAnimation, CharactersGrid },
+        components: {CharactersHeader, SkeletonAnimation, CharactersGrid},
         computed: {
             ...mapGetters({ characters: 'getCharacters' }),
         },
@@ -29,44 +33,48 @@
 </script>
 
 <style lang="scss">
-.main {
-    width: 100%;
-    flex-grow: 1;
-}
-.banner-box {
-    display: flex;
-    justify-content: center;
-    background-image: url('../img/banner.png');
-    background-repeat: no-repeat;
-    max-height: 800px;
-    max-width: 1920px;
-    padding: 351px 0 359px 0;
-    margin: 0 auto;
-}
-.search-box {
-    padding: 20px;
-    background-color: $white;
-    border-radius: $searchBorderRadius;
-    box-shadow: $boxInputShadow;
-}
-.search-string {
-    width: 502px;
-    padding: 17px 0 17px 16px;
-    margin: 0 20px 0 0;
-    background-color: $grey;
-    border-radius: $searchBorderRadius;
-    border: $inputBorder;
-    color: $red;
-}
-.search-button {
-    width: 220px;
-    padding: 18px 20px;
-    text-align: center;
-    background: $red;
-    border: none;
-    border-radius: $searchBorderRadius;
-    color: $white;
-    text-transform: uppercase;
-    font-size: $buttonSize;
-}
+    .main {
+        width: 100%;
+        flex-grow: 1;
+    }
+
+    .banner-box {
+        display: flex;
+        justify-content: center;
+        background-image: url('../img/banner.png');
+        background-repeat: no-repeat;
+        max-height: 800px;
+        max-width: 1920px;
+        padding: 351px 0 355px 0;
+        margin: 0 auto;
+    }
+
+    .search-box {
+        padding: 20px;
+        background-color: $white;
+        border-radius: $searchBorderRadius;
+        box-shadow: $boxInputShadow;
+    }
+
+    .search-string {
+        width: 502px;
+        padding: 17px 0 17px 16px;
+        margin: 0 20px 0 0;
+        background-color: $grey;
+        border-radius: $searchBorderRadius;
+        border: $inputBorder;
+        color: $red;
+    }
+
+    .search-button {
+        width: 220px;
+        padding: 18px 20px;
+        text-align: center;
+        background: $red;
+        border: none;
+        border-radius: $searchBorderRadius;
+        color: $white;
+        text-transform: uppercase;
+        font-size: $buttonSize;
+    }
 </style>
